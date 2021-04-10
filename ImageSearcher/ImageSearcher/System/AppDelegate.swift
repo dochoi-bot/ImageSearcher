8c07e2ee48fe88dd74c0b803539345eaea017bcf
dochoi <dochoi@student.42seoul.kr>
2021-04-11 00:25:40 +0900

    프로젝트 초기 설정

---
+++ ImageSearcher/ImageSearcher/System/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  ImageSearcher
+//
+//  Created by 최동규 on 2021/04/09.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
---
+++ ImageSearcher/ImageSearcher/UI/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ImageSearcher
+//
+//  Created by 최동규 on 2021/04/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Image Searcher"
+        self.navigationItem.searchController = UISearchController()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
